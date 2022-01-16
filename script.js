020cf4d36d00b843bf716cce31b5a28a53fcbd14
chasem <cpmcquown@gmail.com>
2022-01-15 16:48:01 -0800

    adds additional logic

--- script.js
+++ script.js
@@ -5,18 +5,52 @@ const container = document.querySelector("#targeted-btn")
 function inputHandler(e) {
 
     if (e.target.id === "h1-btn") {
-        let hOneValue = document.querySelector("#h1-value").value.trim()
-        document.querySelector("#h1-length").innerHTML = hOneValue.length
+
+        let hOneValueLength = document.querySelector("#h1-value").value.trim().length
+        
+        if (hOneValueLength < 20) {
+            document.querySelector("#h1-length").innerHTML = "Character count: " + hOneValueLength + " ...TO SHORT!"
+        }
+        if (hOneValueLength > 70) {
+            document.querySelector("#h1-length").innerHTML = "Character count: " + hOneValueLength + " ...TO LONG!"
+        }
+        if (hOneValueLength > 20 && hOneValueLength < 70) {
+            document.querySelector("#h1-length").innerHTML = "Character count: " + hOneValueLength + " ...Just Right!"
+        }
+    
+
     }
 
     if (e.target.id === "meta-btn") {
-        let metaValue = document.querySelector("#meta-value").value.trim()
-        document.querySelector("#desc-length").innerHTML = metaValue.length
+
+        let metaValueLength = document.querySelector("#meta-value").value.trim().length
+
+        if (metaValueLength < 120) {
+            document.querySelector("#desc-length").innerHTML = "Character count: " + metaValueLength + " ...TO SHORT!"
+        }
+        if (metaValueLength > 170) {
+            document.querySelector("#desc-length").innerHTML = "Character count: " + metaValueLength + " ...TO LONG!"
+        }
+        if (metaValueLength > 120 && metaValueLength < 170) {
+            document.querySelector("#desc-length").innerHTML = "Character count: " + metaValueLength + " ...Just Right!"
+        }
+        
     }
 
     if (e.target.id === "alt-btn") {
-        let altValue = document.querySelector("#alt-value").value.trim()
-        document.querySelector("#alt-length").innerHTML = altValue.length
+
+        let altValueLength = document.querySelector("#alt-value").value.trim().length
+
+        if (altValueLength < 20) {
+            document.querySelector("#alt-length").innerHTML = "Character count: " + altValueLength + " ...TO SHORT!"
+        }
+        if (altValueLength > 125) {
+            document.querySelector("#alt-length").innerHTML = "Character count: " + altValueLength + " ...TO LONG!"
+        }
+        if (altValueLength > 20 && altValueLength < 125) {
+            document.querySelector("#alt-length").innerHTML = "Character count: " + altValueLength + " ...Just Right!"
+        }
+    
     }
 }
 
